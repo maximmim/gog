da36b61c600df5e8f7da8c2b3bdbd97c48798f52
maximmim <93773088+maximmim@users.noreply.github.com>
2022-02-03 11:49:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+doc = document.cookie = "username=John Smith; expires=Thu, 18 Dec 2013 12:00:00 UTC; path=/";
+function fooop() {
+
+
+let fooof = confirm('тобі сподобався сайт ?')
+
+
+if  (fooof)  {
+    alert('дякую за оцінку ')
+}
+else {
+    alert('дякую за оцінку + создатель грусий ушол')
+}
+
+}  
+   function tot() {
+document.getElementById("if").style.display = "block"
+clearTimeout(nit)
+   }
+   
+function fooopp() {
+
+
+    let foooff = confirm('you liked the site ?')
+    
+    
+    if  (foooff)  {
+        alert('Thanks for the rating ')
+    }
+    else {
+        alert('Thank you for the assessment + the creator of the pear left')
+    }
+    
+    }
+function jojo2 () {
+    document.location.replace('home rus.html')
+}
+function jojo22 () {
+    document.location.replace('home eng.html')
+}
+
+
